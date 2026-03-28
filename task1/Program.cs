87749de14d61b83f7e3507829aeff11455e79ff9
Masha Matsko <mariiamatsko@Mac.local>
2026-03-28 16:21:29 +0200

    third stage

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace OOP_week8_2;
+﻿namespace task1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var file1 = new FileResource("report.txt");
+        var network = new NetworkResource("api.company.local");
+        var manager = new ResourceManager<Resource>();
+        
+        manager.Add(file1);
+        manager.Add(network);
+        
+        manager.OpenAll();
+        
+        using(var file2 = new FileResource("file"))
+        {
+            file2.Open();
+            Console.WriteLine($"Open {file2.Name}");
+        }
+        
+        manager.CloseAll();
+
+        Console.WriteLine("Done.");
     }
 }
