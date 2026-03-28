47c5a3d5ac498030b4e6235a1dbcf2b2ac0943a8
Masha Matsko <mariiamatsko@Mac.local>
2026-03-28 21:16:07 +0200

    stage 3

--- task3_week8/Program.cs
+++ task3_week8/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var container = new DataContainer<DataItem>();
+        for (int i = 0; i < 10000; i++)
+        {
+            var item = new DataItem
+            {
+                Id = $"item-{i}",
+                Payload = new byte[1024]
+            };
+            container.AddItem(item);
+        }
+        Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+        GC.Collect();
+        Console.WriteLine("GC.Collect() was called.");
+        Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
+        
+        // кількість байтів не змінилась тому, що ми користуємось контейнером і він залишається в купі, тому GC його не видаляє.
+        
     }
 }
