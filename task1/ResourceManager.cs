e7afc308add2255412d3b7f61653f11b741cdf60
Masha Matsko <mariiamatsko@Mac.local>
2026-03-28 16:07:26 +0200

    second stage

---
+++ task1/ResourceManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+namespace task1;
+
+public class ResourceManager<T> where T : Resource
+{
+    private readonly List<T> _resources = new();
+
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+    }
+
+    public void OpenAll()
+    {
+        foreach (var resource in _resources)
+        {
+            resource.Open();
+        }
+    }
+
+    public void CloseAll()
+    {
+        foreach (var resource in _resources)
+        {
+            resource.Close();
+        }
+    }
+}
